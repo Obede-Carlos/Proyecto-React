c676eab990c45c7e199dadf13cc3fa8828dbd875
Obede-Carlos <carlosobede8@gmail.com>
2024-10-10 15:29:15 +0000

    commit 1

--- React-app/src/App.tsx
+++ React-app/src/App.tsx
@@ -1,34 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    /**contenedor , fila y una seccion,
+     * con longitud maxima permitida.
+     *  */
+    <div className="container">
+      <div className="row">
+        <section className="col-md-6">
+         <Card><CardBody header="hola mundo" title="titulo del compoennte" text="texto del componente" /></Card>
+         <List />
+       </section>
+     </div>
+    </div>
   )
 }
 
---
+++ React-app/src/components/Card.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+function Card(props: Props) {
+  const { children } = props;
+  return <div className="card">{children}</div>;
+}
+
+interface CardBodyProps {
+  header: string;
+  title: string;
+  text?: string;
+}
+export function CardBody(props: CardBodyProps) {
+  const { header, title, text } = props;
+  return (
+    <>
+      <div className="card-header">{header}</div>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{text}</p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Card;
