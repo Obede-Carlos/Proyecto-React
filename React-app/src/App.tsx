506d682346fe9e16f22eb5f10cd7e5b8ba87cc7e
Obede-Carlos <113922079+Obede-Carlos@users.noreply.github.com>
2024-10-18 11:23:05 +0000

    funciones como props

--- React-app/src/App.tsx
+++ React-app/src/App.tsx
@@ -1,20 +1,32 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
-import './App.css'
 
 function App() {
+  const list = ["Carlos", "lucia", "Mario", "david"];
+
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo ", elemento);
+  }
   return (
     /**contenedor , fila y una seccion,
      * con longitud maxima permitida.
      *  */
-    <div className="container">
-      <div className="row">
-        <section className="col-md-12">
-         <Card><CardBody header="hola mundo" title="titulo del compoennte" text="texto del componente" /></Card>
-         <List />
-       </section>
-     </div>
-    </div>
+    <main>
+      <div className="container-lg">
+        <div className="row">
+          <section className="col-md-12">
+          <Card><CardBody header="hola mundo" title="titulo del compoennte" text="texto del componente" /></Card>
+          </section>
+        </div>
+      </div>
+      <div className="container-lg">
+        <div className="row">
+          <section className="col-md-12">
+          <List data={list} onSelect={handleSelect}/>
+          </section>
+        </div>
+      </div>
+    </main>
   )
 }
 
